2f5ac99162e854371925f442674557eac345ca56
Leman Gahramanova <lemangahramanoval@gmail.com>
2026-05-25 19:41:03 +0400

    user actions is added

---
+++ src/com/youtube/bank/repository/UserRepository.java
@@ -0,0 +1,56 @@
+package com.youtube.bank.repository;
+
+import com.youtube.bank.entity.User;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserRepository {
+
+    private static Set<User> users = new HashSet<>();
+
+    static {
+        User user1 = new User("admin", "admin", "1234567", "admin", 0.0);
+        User user2 = new User("user2", "user2", "12345678", "user", 1000.0);
+        User user3 = new User("user3", "user3", "12345679", "user", 2000.0);
+        User user4 = new User("user3", "user3", "12345679", "user", 2000.0);
+
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+    }
+
+    public Double checkBankBalance(String userId){
+        List<User> result = users.stream().filter(user -> user.getUsername().equals(userId)).collect(Collectors.toList());
+        if(!result.isEmpty()){
+            return result.get(0).getAccountBalance();
+        } else {
+            return null;
+        }
+    }
+
+    public void printUsers(){
+        System.out.println(users);
+    }
+
+    public User login(String username, String password){
+        List<User> finalList = users.stream()
+                .filter(user -> user.getUsername().equals(username) && user.getPassword().equals(password))
+                .collect(Collectors.toList());
+
+        if(!finalList.isEmpty()) {
+            return finalList.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean addNewCustomer(String username, String password, String contact){
+        User user = new User(username, password, contact, "user", 500.0);
+        return users.add(user);
+    }
+
+}
---
+++ src/com/youtube/bank/service/UserService.java
@@ -0,0 +1,23 @@
+package com.youtube.bank.service;
+
+import com.youtube.bank.entity.User;
+import com.youtube.bank.repository.UserRepository;
+
+public class UserService {
+    private UserRepository userRepository = new UserRepository();
+    public void printUsers(){
+        userRepository.printUsers();
+    }
+
+    public User login(String username, String password) {
+        return userRepository.login(username, password);
+    }
+
+    public boolean addNewCustomer(String username, String password, String contact){
+        return userRepository.addNewCustomer(username, password, contact);
+    }
+
+    public Double checkBankBalance(String userId){
+        return userRepository.checkBankBalance(userId);
+    }
+}
